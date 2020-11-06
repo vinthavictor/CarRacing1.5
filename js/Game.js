22cb7568fc81d1428df4bf32447c06a789cbe05b
Vinitha <vinithavictor007@gmail.com>
2020-11-06 11:19:57 +0530

    camera and changes

--- js/Game.js
+++ js/Game.js
@@ -26,31 +26,50 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100,200);
+    car2 = createSprite(300,200);
+    car3 = createSprite(500,200);
+    car4 = createSprite(700,200);
+    cars = [car1,car2,car3,car4]
   }
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
+    // textSize(30);
+    // text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      var index = 0;
+      var x =0;
+      var y;
+      //var display_position = 130;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
+        index=index+1;
+        // if (plr === "player" + player.index)
+        //   fill("red")
+        // else
+        //   fill("black");
+        x=x+200;
+        // display_position+=20;
+        // textSize(15);
+        // text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      y=displayHeight - allPlayers[plr].distance;
+      cars[index-1].x=x;
+      cars[index-1].y=y;
 
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      if(index===player.index){
+        cars[index-1].shapeColor = "red";
+        camera.position.x=displayWidth/2;
+        camera.position.y=cars[index-1].y; //.y
+      }
       }
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
+      player.distance +=10
       player.update();
     }
+    drawSprites();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var canvas, backgroundImage;
 
+
 var gameState = 0;
 var playerCount;
 var allPlayers;
@@ -7,10 +8,10 @@ var distance = 0;
 var database;
 
 var form, player, game;
-
+var cars,car1,car2,car3,car4;
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
